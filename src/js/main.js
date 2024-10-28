d1ead9ca4cca673191232e6a1dbba57ec6e0c0a4
Valeria N <v.567insp@gmail.com>
2024-10-28 15:09:45 +0200

    sections hero and bright

--- src/js/main.js
+++ src/js/main.js
@@ -59,3 +59,19 @@ slider.addEventListener("change", () => {
 })
 
 playMusic();
+
+
+// burger
+(function () {
+	const isOpen = document.querySelector(".header__inner-burger")
+	const isClose = document.querySelector(".header__nav-close")
+	const navigation = document.querySelector(".header__nav")
+
+	isOpen.addEventListener('click', () => {
+		navigation.classList.add('active')
+	})
+
+	isClose.addEventListener('click', () => {
+		navigation.classList.remove('active')
+	})
+})();
